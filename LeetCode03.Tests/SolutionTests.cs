6f883c1d8c78874f50cef8cf4a6d0cb8c703fea1
givemin5 <givemin5@gmail.com>
2018-01-17 10:42:09 +0800

    第二支測試通過

--- LeetCode03.Tests/SolutionTests.cs
+++ LeetCode03.Tests/SolutionTests.cs
@@ -12,7 +12,7 @@ namespace LeetCode03.Tests
     public class SolutionTests
     {
         [TestMethod()]
-        public void LengthOfLongestSubstringTest_abcabcbb_ShouldBe_abc()
+        public void LengthOfLongestSubstringTest_abcabcbb_ShouldBe_3()
         {
             var input = "abcabcbb";
             Solution solution = new Solution();
@@ -20,6 +20,20 @@ namespace LeetCode03.Tests
             var actual = solution.LengthOfLongestSubstring(input);
 
             var expected = 3;
+
+            Assert.AreEqual(expected, actual);
+
+
+        }
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest_bbbbb_ShouldBe_1()
+        {
+            var input = "bbbbb";
+            Solution solution = new Solution();
+
+            var actual = solution.LengthOfLongestSubstring(input);
+
+            var expected = 1;
 
             Assert.AreEqual(expected, actual);
 
